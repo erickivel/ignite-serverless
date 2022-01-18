bc88e93804805bb76a4497945df33aa1ae870f8e
Eric Kivel <ericvkivel1@gmail.com>
2022-01-18 19:21:17 -0300

    Serverless

---
+++ src/functions/hello.ts
@@ -0,0 +1,15 @@
+
+
+
+export const handle = async (event) => {
+
+    return {
+        statusCode: 201,
+        body: JSON.stringify({
+            message: "Hello World Ignite Serverless"
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        }
+    };
+};
